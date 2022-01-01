790a4d2860e77e5a2ec87291d456445c9173066b
Vinsensius Danny <vinsensius.danny@gmail.com>
2022-01-01 21:59:19 +0700

    Update cv download path

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     title: 'Vinsensius Danny - Fullstack Developer from Indonesia',
     avatar: 'https://res.cloudinary.com/vdanny/image/upload/c_scale,w_300/v1566313285/portfolio/me_sc2bbg.jpg',
     // cvUrl: 'http://localhost:8000/Vinsensius_Danny_Resume.pdf',
-    cvUrl: 'http://vdanny.com/Vinsensius_Danny_Resume.pdf',
+    cvUrl: 'https://vdanny.github.io/Vinsensius_Danny_Resume.pdf',
     careerStart: 2014
   },
   plugins: [
